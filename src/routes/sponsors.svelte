d4ba54245ccc180bd30820ebcf5590e47b5119f2
Travis Boss <travisboss@gmail.com>
2022-06-16 13:28:37 -0500

    added sponsor banner

--- src/routes/sponsors.svelte
+++ src/routes/sponsors.svelte
@@ -2,29 +2,36 @@
 	<title>Sponsor</title>
 </svelte:head>
 
-<img
-	src="https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2021/06/Screen-Shot-2021-06-14-at-2.10.01-PM.png"
-	class="pt-6 aspect-auto"
-/>
+<div class="flex justify-center pt-6">
+	<a href="https://ibb.co/8zbvrFb"
+		><img
+			src="https://i.ibb.co/TK1j4z1/Fireworks-Sponsor-Banner-Outsource1024-1.jpg"
+			alt="Fireworks Sponsor Banner"
+			border="0"
+		/></a
+	>
+</div>
 
 <div class="hero pt-6">
 	<div class="hero-contnet">
-		<p class="p-1">
-			Funded entirely through private contributions, the St. Cloud July 4th Fireworks needs your
-			help to continue.
-		</p>
+		<div class="max-w-4xl">
+			<p class="p-1">
+				Funded entirely through private contributions, the St. Cloud July 4th Fireworks needs your
+				help to continue.
+			</p>
 
-		<p class="p-1">
-			Each year, private donations, corporate sponsorships, and volunteer efforts provide the
-			support necessary to “Light up the Sky on the Fourth of July”.
-		</p>
+			<p class="p-1">
+				Each year, private donations, corporate sponsorships, and volunteer efforts provide the
+				support necessary to “Light up the Sky on the Fourth of July”.
+			</p>
 
-		<p class="p-1">
-			In order to make this event a success, we need your assistance. All contributions large or
-			small are graciously accepted. <span class="font-bold"
-				>Checks can be made payable to Fireworks Fund</span
-			>. See donate page on where to send check or contact us.
-		</p>
-		<h2 class="text-3xl mb-5">Donate to keep the tradition strong</h2>
+			<p class="p-1">
+				In order to make this event a success, we need your assistance. All contributions large or
+				small are graciously accepted. <span class="font-bold"
+					>Checks can be made payable to Fireworks Fund</span
+				>. See donate page on where to send check or contact us.
+			</p>
+			<h2 class="text-3xl mb-5">Donate to keep the tradition strong</h2>
+		</div>
 	</div>
 </div>
