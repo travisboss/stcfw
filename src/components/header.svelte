9d0db62b04871c1925621838685b208706ca1e99
Travis Boss <travisboss@gmail.com>
2022-06-27 08:46:26 -0500

    fixed menu item on safari

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -16,18 +16,7 @@
 	<div class="navbar-start">
 		<div class="dropdown">
 			<label tabindex="0" class="btn btn-ghost btn-circle">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h16M4 18h7"
-					/></svg
-				>
+				<img src="https://i.ibb.co/pX8FtVc/menu.png" alt="menu" />
 			</label>
 			<ul
 				tabindex="0"
