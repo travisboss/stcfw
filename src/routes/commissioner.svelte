b795bd0c4615fbe08afaf91adf1d2f2721daa027
Travis Boss <travisboss@gmail.com>
2022-07-04 10:48:40 -0500

    added commissioner video

--- src/routes/commissioner.svelte
+++ src/routes/commissioner.svelte
@@ -5,15 +5,21 @@
 <div class="hero min-h-0 bg-base-200">
 	<div class="hero-content text-center">
 		<div class="max-w-xxl">
+			<a
+				class="link link-neutral my-6"
+				href="https://app.frame.io/presentations/dc276c7c-90b8-4bc5-8f89-6510a2949e73"
+				>2022 Honorary Fireworks Commissioner (click to view)</a
+			>
 			<p>
-				The Honorary Fireworks Commissioner title recognizes area businesses, residents and
-				organizations that embrace the importance of America’s Independence Day by helping make
-				possible one of the largest July 4th pyrotechnic displays and celebrations in the upper
-				Midwest and by embodying the spirit, tradition and significance of America’s founding, and
-				the commitment to continue to ensure the ideals of our founding fathers. Honorary Fireworks
-				Commissioners have the honor of pushing the button that begins the pyrotechnics on July 4th.
-				They also represent the Fireworks Committee by riding on the St. Cloud Fire Department’s
-				antique Ahrens Fox Firetruck in the Granite City Days parade in June.
+				<br />
+				The Honorary Fireworks Commissioner title recognizes area businesses, residents and organizations
+				that embrace the importance of America’s Independence Day by helping make possible one of the
+				largest July 4th pyrotechnic displays and celebrations in the upper Midwest and by embodying
+				the spirit, tradition and significance of America’s founding, and the commitment to continue
+				to ensure the ideals of our founding fathers. Honorary Fireworks Commissioners have the honor
+				of pushing the button that begins the pyrotechnics on July 4th. They also represent the Fireworks
+				Committee by riding on the St. Cloud Fire Department’s antique Ahrens Fox Firetruck in the Granite
+				City Days parade in June.
 			</p>
 			<p class="py-6">Congratulations 2022 Honorary Fireworks Commissioner: Dennis Schiffler!</p>
 			<a class="link link-neutral py-6" href="https://ibb.co/2dQs3QQ">Press Release</a>
