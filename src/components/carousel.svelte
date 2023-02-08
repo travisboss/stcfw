0a7877c4961f818efd1164b4ce9ac7ef3d776eef
Travis <travisrboss@pm.me>
2023-02-08 15:14:32 -0600

    remove swiper dep and move to TS

--- src/components/carousel.svelte
+++ src/components/carousel.svelte
@@ -1,43 +1,45 @@
-<script>
-	// @ts-nocheck
+<script lang="ts">
+  import { onMount } from 'svelte';
 
-	import { Swiper, SwiperSlide } from 'swiper/svelte';
+  type Slide = {
+    img: string;
+    alt: string;
+  };
 
-	// Import Swiper styles
-	import 'swiper/css';
+  let slides: Slide[] = [
+    {
+      img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-25-1400x400.jpg',
+      alt: '',
+    },
+    {
+      img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-7-1400x400.jpg',
+      alt: '',
+    },
+    {
+      img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-18-1400x400.jpg',
+      alt: '',
+    },
+    {
+      img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-21-1400x400.jpg',
+      alt: '',
+    },
+  ];
 
-	let slides = [
-		{
-			img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-25-1400x400.jpg',
-			alt: ''
-		},
-		{
-			img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-7-1400x400.jpg',
-			alt: ''
-		},
-		{
-			img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-18-1400x400.jpg',
-			alt: ''
-		},
-		{
-			img: 'https://secureservercdn.net/166.62.112.107/e4e.0a7.myftpupload.com/wp-content/uploads/2019/02/Fireworks-21-1400x400.jpg',
-			alt: ''
-		}
-	];
+  let currentIndex = 0;
 
-	import { Autoplay } from 'swiper';
+  onMount(() => {
+    const interval = setInterval(() => {
+      currentIndex = (currentIndex + 1) % slides.length;
+    }, 6000);
+
+    return () => clearInterval(interval);
+  });
 </script>
 
 <div class="carousel w-full">
-	<Swiper
-		autoplay={{
-			delay: 6000
-		}}
-		modules={[Autoplay]}
-		class="mySwiper"
-	>
-		{#each slides as { img, alt }}
-			<SwiperSlide><img src={img} {alt} class="w-full aspect-auto" /></SwiperSlide>
-		{/each}
-	</Swiper>
+  {#each slides as slide, index}
+    {#if index === currentIndex}
+      <img src={slide.img} alt={slide.alt} class="w-full aspect-auto" />
+    {/if}
+  {/each}
 </div>
