7e43084083aea6ac243466c96beb960d5079f1e0
Travis <travisrboss@pm.me>
2023-02-08 19:15:58 -0600

    revert to js file with jsdoc style

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { imagetools } from 'vite-imagetools';
+
+/**
+ * @typedef {import('@sveltejs/kit').Config} Config
+ */
+
+/** @type {Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter()
+	},
+
+	vite: {
+		plugins: [imagetools()]
+	}
+};
+
+module.exports = config;
